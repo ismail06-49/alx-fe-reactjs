82e4c2b4061ff5a291532fc1351ec778e8fce934
ismail haddad <eaglesismail59@gmail.com>
2024-11-07 14:28:43 +0100

    update

---
+++ alx-react-app-new/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer>
+            <p style={{fontSize: '24px', color: 'navy'}}>© 2023 City Lovers</p>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ alx-react-app-new/src/components/MainContent.jsx
@@ -0,0 +1,9 @@
+function MainContent() {
+    return (
+        <main>
+            <p style={{fontSize: '48px', fontWeight: 'bold', textDecoration: 'underline green'}}>I love to visit New York, Paris, and Tokyo.</p>
+        </main>
+    );
+}
+
+export default MainContent;
