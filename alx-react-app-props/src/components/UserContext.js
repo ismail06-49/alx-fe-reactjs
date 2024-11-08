d4d3d5fef3792582338375aae4abdd8bc1a0c10b
ismail haddad <eaglesismail59@gmail.com>
2024-11-08 15:21:14 +0100

    update

--- alx-react-app-props/src/components/UserContext.js
+++ alx-react-app-props/src/components/UserContext.js
@@ -1,3 +1,12 @@
 import React from "react";
 
-export const UserContext = React.createContext({});
+export const UserContext = React.createContext();
+
+export default function UserProvider({ children }) {
+
+    return (
+        <UserContext.Provider >
+            {children}
+        </UserContext.Provider>
+    )
+}
