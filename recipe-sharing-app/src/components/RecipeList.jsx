3a69fb6d5055228b066fcad5f08e5549336bce71
ismail haddad <eaglesismail59@gmail.com>
2024-11-12 13:56:57 +0100

    update

--- recipe-sharing-app/src/components/RecipeList.jsx
+++ recipe-sharing-app/src/components/RecipeList.jsx
@@ -2,13 +2,15 @@ import { useRecipeStore } from './recipeStore';
 
 const RecipeList = () => {
     const recipes = useRecipeStore(state => state.recipes);
-
+    const deleteRecipe = useRecipeStore(state => state.deleteRecipe);
+    
     return (
         <div>
             {recipes.map(recipe => (
             <div key={recipe.id}>
                 <h3>{recipe.title}</h3>
                 <p>{recipe.description}</p>
+                <button onClick={() => deleteRecipe(recipe.id)}>Delete</button>
             </div>
             ))}
         </div>
--- recipe-sharing-app/src/components/recipeStore.js
+++ recipe-sharing-app/src/components/recipeStore.js
@@ -3,5 +3,10 @@ import { create } from 'zustand'
 export const useRecipeStore = create(set => ({
     recipes: [],
     addRecipe: (newRecipe) => set(state => ({ recipes: [...state.recipes, newRecipe] })),
+    deleteRecipe: (recipeId) =>
+        set(state => ({
+            recipes: state.recipes.filter((recipe) => recipe.id !== recipeId)
+        })),
+    updateRecipe:'',
     setRecipes: (recipes) => set({ recipes })
 }))
