58e72f77cd165fc54f3d337d7e093430a283eb48
ismail haddad <eaglesismail59@gmail.com>
2024-11-12 12:23:37 +0100

    add recipe-sharing-app

---
+++ recipe-sharing-app/src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import AddRecipeForm from './components/AddRecipeForm'
+import RecipeList from './components/RecipeList'
+
+function App() {
+
+  return (
+    <>
+      <RecipeList />
+      <AddRecipeForm />
+    </>
+  )
+}
+
+export default App
