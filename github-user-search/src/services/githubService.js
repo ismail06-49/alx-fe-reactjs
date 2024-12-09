148380ea8e22819bf1c45c0a06fbc7fe8e569280
ismail haddad <eaglesismail59@gmail.com>
2024-12-10 00:12:35 +0100

    API integration

--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 export async function fetchUserData(term) {
 
     const options = {
-        method: 'GET',
+        method: 'get',
         url: `https://api.github.com/users/${term}`,
     };
 
