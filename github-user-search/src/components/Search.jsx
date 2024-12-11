cb45b06dfb7ea25050c77d9dffc8d10a43a3fde3
ismail haddad <eaglesismail59@gmail.com>
2024-12-11 13:46:01 +0100

    Enhancing User Interface and Adding Advanced Search Features

--- github-user-search/src/components/Search.jsx
+++ github-user-search/src/components/Search.jsx
@@ -1,52 +1,57 @@
-import { useEffect, useState } from "react";
+import {  useState } from "react";
 import { fetchUserData } from "../services/githubService";
 
 export default function Search() {
 
-    const [search, setSearch] = useState('');
-    const [user, setUser ] = useState(null);
+    const [user, setUser] = useState('');
+    const [location, setLocation] = useState('');
+    const [repo, setRepo] = useState('');
+    const [users, setUsers ] = useState(null);
     const [loading, setLoading] = useState(false);
     const [error, setError] = useState(false);
     const [login, setLogin] = useState(false)
 
     function handleChange(e) {        
-        e.preventDefault
-    }
-
-    useEffect(() => {
+        e.preventDefault();
         async function getData() {
-            if (!search) return; // Don't fetch if search is empty
+            if (!user && !location && !repo) return; 
             setLoading(true);
             setError(false);
             try {
-                const data = await fetchUserData(search);
-                setUser(data);
+                const data = await fetchUserData(user, location, repo);
+                setUsers(data);
+                console.log(data);
+                console.log(users);
                 setLogin(true)
             } catch (err) {
                 setError(true);
-                console.log('Looks like we cant find the user');
+                console.log(`Looks like we cant find the user: ${err}`);
                 
             } finally {
                 setLoading(false);
             }
         }
         getData();
-    },[search])
+    }
 
     return (
         <div>
             <form onSubmit={(e) => handleChange(e)} className=" w-3/4 mt-5 mx-auto flex flex-row">
-                <input type="text" className="py-3 px-4 block w-3/4 border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" placeholder="Search..." onChange={(e) => setSearch(e.target.value)} />
-                <button className="w-1/4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg" type="submit">Refresh</button>
+                <input type="text" className="py-3 px-4 block w-1/4 border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" placeholder="Search..." onChange={(e) => setUser(e.target.value)} />
+                <input type="text" className="py-3 px-4 block w-1/4 border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" placeholder="Location..." onChange={(e) => setLocation(e.target.value)} />
+                <input type="text" className="py-3 px-4 block w-1/4 border-gray-200 rounded-lg text-sm focus:border-blue-500 focus:ring-blue-500 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-900 dark:border-neutral-700 dark:text-neutral-400 dark:placeholder-neutral-500 dark:focus:ring-neutral-600" placeholder="Min Repo..." defaultValue={repo} onChange={(e) => setRepo(e.target.value)} />
+                <button className="w-1/4 bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-lg" type="submit">Search</button>
             </form>
             {loading && <p>Loading...</p>}
             {error && <p>Looks like we cant find the user</p>}
             {login && (
-                <div className="flex flex-col w-1/4 mx-auto mt-4 bg-white border shadow-sm rounded-xl p-4 md:p-5 dark:bg-neutral-900 dark:border-neutral-700 dark:shadow-neutral-700/70">
-                    <h2 className="text-lg text-center font-bold text-gray-800 dark:text-white">{user.name}</h2>
-                    <img className="mx-auto" src={user.avatar_url} alt={`${user.name}'s avatar`} width="100" />
-                    <a className="mt-3 text-center gap-x-1 text-sm font-semibold rounded-lg border border-transparent text-blue-600 decoration-2 hover:text-blue-700 hover:underline focus:underline focus:outline-none focus:text-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:text-blue-500 dark:hover:text-blue-600 dark:focus:text-blue-600" href={user.html_url} target="_blank" rel="noopener noreferrer">View Profile</a>
-                </div>
+                users.map((per) => (
+                    <div key={per.id} className="flex flex-col w-1/4 mx-auto mt-4 bg-white border shadow-sm rounded-xl p-4 md:p-5 dark:bg-neutral-900 dark:border-neutral-700 dark:shadow-neutral-700/70">
+                        <h2 className="text-lg text-center font-bold text-gray-800 dark:text-white">{per.login}</h2>
+                        <img className="mx-auto" src={per.avatar_url} alt={`${per.login}'s avatar`} width="100" />
+                        <a className="mt-3 text-center gap-x-1 text-sm font-semibold rounded-lg border border-transparent text-blue-600 decoration-2 hover:text-blue-700 hover:underline focus:underline focus:outline-none focus:text-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:text-blue-500 dark:hover:text-blue-600 dark:focus:text-blue-600" href={per.html_url} target="_blank" rel="noopener noreferrer">View Profile</a>
+                    </div>
+                ))
             )}
         </div>
     )
--- github-user-search/src/services/githubService.js
+++ github-user-search/src/services/githubService.js
@@ -1,16 +1,22 @@
 import axios from 'axios';
 // console.log(process.env.REACT_APP_GITHUB_API_KEY);
 
-export async function fetchUserData(term) {
+export async function fetchUserData(user, location, repo) {
 
+    const query = `${user} ${location ? `+location:${location}` : ''} ${repo ? `+repos:>=${repo}` : ''}`.trim();
     const options = {
         method: 'get',
-        url: `https://api.github.com/users/${term}`,
+        url: `https://api.github.com/search/users?q=${query}`,
     };
+
+    // const options = {
+    //     method: 'get',
+    //     url: `https://api.github.com/users/${term}`,
+    // };
 
     try {
         const response = await axios.request(options);
-        return response.data;
+        return response.data.items;
     } catch (error) {
         console.error(error);
     }
